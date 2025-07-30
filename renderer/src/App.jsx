2517fae7b3b7e594d32a3e5c40b1302d71a5c93e
vivek Desale <60285597+vivekdesale54@users.noreply.github.com>
2025-07-30 19:06:26 +0530

    changes

---
+++ renderer/src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '4rem' }}>
+      <h1>Hello from React + Electron + Vite!</h1>
+    </div>
+  );
+}
